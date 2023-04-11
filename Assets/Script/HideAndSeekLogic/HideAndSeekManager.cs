16ab2b31851cebe3cb09fe60cd5e8d2b8bc82cde
Quentin <quentin.dubois@etu.univ-cotedazur.fr>
2023-04-11 10:40:50 +0200

    ajout de l'indication du nombre de créatures restantes à trouver

--- Assets/Script/HideAndSeekLogic/HideAndSeekManager.cs
+++ Assets/Script/HideAndSeekLogic/HideAndSeekManager.cs
@@ -1,4 +1,5 @@
 using System;
+using TMPro;
 using UnityEngine;
 
 public class HideAndSeekManager: MonoBehaviour
@@ -7,16 +8,29 @@ public class HideAndSeekManager: MonoBehaviour
     private bool[] creaturesDiscovered;
     public Dialogue dialogueAllCreaturesDiscovered;
     public Planet3Script planet3Script;
+    private TextMeshProUGUI _textCreatureDiscovered;
+    private int _creaturesFound = 0;
 
     public void Start()
     {
         creaturesDiscovered = new bool[creaturesNameToDiscover.Length];
         planet3Script = GameObject.Find("GlobalManager").GetComponent<Planet3Script>();
+        _textCreatureDiscovered = GameObject.Find("CreaturesFound").GetComponent<TextMeshProUGUI>();
+        _textCreatureDiscovered.enabled = false;
+    }
+    
+    public void StartHideAndSeek()
+    {
+        Debug.Log("Start Hide and Seek");
+        _textCreatureDiscovered.enabled = true;
+        _updateTextCreaturesFound();
     }
 
     public void CreatureDiscovered(string creatureNameDiscovered)
     {
         Debug.Log("Creature discovered: " + creatureNameDiscovered);
+        _creaturesFound++;
+        _updateTextCreaturesFound();
         foreach (string creatureName in creaturesNameToDiscover)
         {
             if (creatureName == creatureNameDiscovered)
@@ -38,10 +52,18 @@ public class HideAndSeekManager: MonoBehaviour
         }
         Debug.Log("All creatures discovered");
         
+        // Desactivate Text UI
+        _textCreatureDiscovered.enabled = false;
+        
         // Trigger dialogue
         FindObjectOfType<DialogueManager>().StartDialogue(dialogueAllCreaturesDiscovered);
         
         // Call the methode in Planet3Script
         planet3Script.AllCreaturesDiscovered();
+    }
+
+    private void _updateTextCreaturesFound()
+    {
+        _textCreatureDiscovered.text = $"Creatures trouvees: {_creaturesFound}/{creaturesNameToDiscover.Length}";
     }
 }
