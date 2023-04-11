db6da4a1d5fd43690a210c0cf36a420575589920
Sami EL KATEB <sami.elkateb@gmail.com>
2023-04-11 02:34:30 +0200

    fix: 3rd person control

--- Assets/Script/ThirdPersonCam.cs
+++ Assets/Script/ThirdPersonCam.cs
@@ -66,11 +66,48 @@ public class ThirdPersonCam : MonoBehaviour
 // }
 void Update() {
     // Get keyboard input
-    // float horizontalInput = Input.GetAxis("Horizontal");
-    float horizontalInputMouse = Input.GetAxis("Mouse X") * 110;
+    float horizontalInput = Input.GetAxis("Horizontal");
+    float horizontalInputMouse = Input.GetAxis("Mouse X") * rotationSpeed;
 
     Vector3 rotationVector = new Vector3(0, horizontalInputMouse, 0);
 
     transform.Rotate(rotationVector * rotationSpeed * Time.deltaTime, Space.Self);
+
+    // playerGameObject.transform.rotation = Camera.main.transform.rotation;
+
+    // GameObject player = FindObjectOfType<Player>().gameObject;
+    // player.transform.LookAt(new Vector3(target.transform.position.x, player.transform.position.y, player.transform.position.z));
+        // Transform selfTransform = player.transform;
+        //  float distanceToPlane = Vector3.Dot(selfTransform.up, target.transform.position - selfTransform.position);
+        //  Vector3 pointOnPlane = target.transform.position - (selfTransform.up * distanceToPlane);
+        //  Debug.Log(pointOnPlane.x);
+        //  selfTransform.LookAt(pointOnPlane, selfTransform.up);
+
+        // float distanceToPlane = Vector3.Dot(selfTransform.up, position - selfTransform.position);
+        //  Vector3 pointOnPlane = position - (selfTransform.up * distanceToPlane);
+        //  selfTransform.LookAt(pointOnPlane, selfTransform.up);
+
+    }
+    
+    void FixedUpdate(){
+        // Transform selfTransform = player.transform;
+        // float distanceToPlane = Vector3.Dot(selfTransform.up, target.transform.position - selfTransform.position);
+        // Vector3 pointOnPlane = target.transform.position - (selfTransform.up * distanceToPlane);
+        // Debug.Log(pointOnPlane.x);
+        // //  selfTransform.LookAt(pointOnPlane, selfTransform.up);
+        // float speed = 10f;
+        // Vector3 direction = target.transform.position - selfTransform.position;
+        // Quaternion toRotation = Quaternion.FromToRotation(selfTransform.forward, pointOnPlane);
+        // transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, speed * Time.time);
+        // Vector3 direction = target.transform.position - transform.position;
+        // direction.y = 0; // Ignore vertical (Y) direction to only rotate on local axis
+        // rotationSpeed = 1.0f;
+
+        // // Rotate the player to face the camera direction
+        // if (direction.magnitude > 0.1f) // Only rotate if there is a significant direction
+        // {
+        //     Quaternion rotation = Quaternion.LookRotation(direction);
+        //     transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
+        // }
     }
 }
