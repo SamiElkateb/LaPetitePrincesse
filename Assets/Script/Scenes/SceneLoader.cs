902b814e88583108c71422fc4edb7f6195467b85
Sami EL KATEB <sami.elkateb@gmail.com>
2023-04-18 19:16:45 +0200

    fix: planet 3 lighting

--- Assets/Script/Scenes/SceneLoader.cs
+++ Assets/Script/Scenes/SceneLoader.cs
@@ -6,30 +6,31 @@ public class SceneLoader : MonoBehaviour
     
     public void LoadMenu()
     {
-        SceneManager.LoadScene("StartMenu");
+        SceneManager.LoadScene("StartMenu", LoadSceneMode.Single);
+
     }
     public void LoadPlanet1()
     {
-        SceneManager.LoadScene("Planet1");
+        SceneManager.LoadScene("Planet1", LoadSceneMode.Single);
     }
 
     public void LoadMuseum()
     {
-        SceneManager.LoadScene("Museum");
+        SceneManager.LoadScene("Museum", LoadSceneMode.Single);
     }
 
     public void LoadRace()
     {
-        SceneManager.LoadScene("Planet2");
+        SceneManager.LoadScene("Planet2", LoadSceneMode.Single);
     }
 
     public void LoadHideAndSeek()
     {
-        SceneManager.LoadScene("Planet3");
+        SceneManager.LoadScene("Planet3", LoadSceneMode.Single);
     }
     
     public void LoadEndGame()
     {
-        SceneManager.LoadScene("EndScreen");
+        SceneManager.LoadScene("EndScreen", LoadSceneMode.Single);
     }
 }
